40b6fc2ecd3215f7a16fefc5fbd1d0e2e0d98f7a
Leon Valencia <techie@lynx.rocks>
2021-05-03 08:21:39 +0800

    use us-east-1 for front-end web and its pipeline

--- lib/angular-site-stage.ts
+++ lib/angular-site-stage.ts
@@ -9,13 +9,20 @@ export class AngularSiteStage extends Stage {
 
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
-    const website = new CdnStack(this, 'Website');
+    const webEnv = {
+      region: 'us-east-1', // use us-east-1 to allow easy CloudFront integration
+    }    
+    const website = new CdnStack(this, 'Website', {
+      env: webEnv,
+    });
     new GithubLinuxCdnPipelineStack(this, 'AngularPipeline', {
       githubTokenName: 'github-token',
       githubOwner: 'engr-lynx',
       githubRepo: 'angular-sample',
       s3Bucket: website.sourceBucket,
       distributionId: website.distributionId,
+    }, {
+      env: webEnv,
     });
   }
 
