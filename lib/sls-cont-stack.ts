537d4b9338a5f64618f68b882536165c186eaa1d
Leon Valencia <techie@lynx.rocks>
2021-05-18 09:10:09 +0800

    fix incorrect lambda creation

--- lib/sls-cont-stack.ts
+++ lib/sls-cont-stack.ts
@@ -3,7 +3,7 @@ import { Construct, Stack, StackProps } from '@aws-cdk/core';
 import { Vpc } from '@aws-cdk/aws-ec2';
 import { AuthorizationType } from '@aws-cdk/aws-apigateway';
 import { ApiGatewayToLambda } from '@aws-solutions-constructs/aws-apigateway-lambda';
-import { Runtime, Code } from '@aws-cdk/aws-lambda';
+import { DockerImageFunction, DockerImageCode } from '@aws-cdk/aws-lambda';
 import { RetentionDays } from '@aws-cdk/aws-logs';
 
 export interface SlsContProps extends StackProps {
@@ -14,14 +14,12 @@ export class SlsContStack extends Stack {
 
   constructor(scope: Construct, id: string, slsContProps: SlsContProps) {
     super(scope, id, slsContProps);
-    const contCode = Code.fromDockerBuild(join(__dirname, 'sls-cont-dummy'));
-    const lambdaProps = {
-      runtime: Runtime.PYTHON_3_8,
-      handler: 'app.handler',
-      code: contCode,
+    const lambdaCode = DockerImageCode.fromImageAsset(join(__dirname, 'sls-cont-dummy'));
+    const lambdaObj = new DockerImageFunction(this, 'LambdaObj', {
+      code: lambdaCode,
       vpc: slsContProps.vpc,
       logRetention: RetentionDays.ONE_DAY,
-    }
+    });
     const methodOpts = {
       // ToDo: change type to custom or Cognito once the auth mechanism is added
       authorizationType: AuthorizationType.NONE,
@@ -30,7 +28,7 @@ export class SlsContStack extends Stack {
       defaultMethodOptions: methodOpts,
     }
     new ApiGatewayToLambda(this, 'Serverless', {
-      lambdaFunctionProps: lambdaProps,
+      existingLambdaObj: lambdaObj,
       apiGatewayProps,
     });
   }
